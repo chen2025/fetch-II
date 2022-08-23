671f31b18493589f5edb0d0cb5b97468a1d9b785
Tony <yc980@georgetown.edu>
2022-08-23 17:20:35 -0400

    initial-commit

---
+++ fetch-requests.js
@@ -0,0 +1,64 @@
+/*
+Make fetch requests in the browser for each of the following phases.
+Paste your code for fetch requests here once you finish each phase.
+*/
+
+/* ============================== Phase 1 ============================== */
+
+// Your code here
+fetch("/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+});
+
+
+/* ============================== Phase 2 ============================== */
+
+// Your code here
+fetch("/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+}).then((res) => {console.log(res.status)});
+
+fetch("/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+}).then(function(res) {
+    console.log(res.headers.get('Content-Type'));
+});
+
+fetch("/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+}).then(function(res) {
+    console.log(res.url);
+});
+
+
+/* ============================== Phase 3 ============================== */
+
+// Your code here
+fetch("/products", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams({
+        name: "Caribbean Delight Coffee",
+        description: "Made by Manatee Coffee",
+        price: 11.99,
+        categories: "grocery"
+    })
+});
